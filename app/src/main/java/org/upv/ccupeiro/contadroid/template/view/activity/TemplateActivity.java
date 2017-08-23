8e8ceaf1e4c6acc49c185bcccbfb4e87360370c6
Carlos Casa <carlos.cupeiro@grupocmc.es>
2017-08-23 21:00:20 +0200

    create activity and set style with no animation to activities

---
+++ app/src/main/java/org/upv/ccupeiro/contadroid/template/view/activity/TemplateActivity.java
@@ -0,0 +1,12 @@
+package org.upv.ccupeiro.contadroid.template.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TemplateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
